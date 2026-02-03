b7894576560e8a472b753d863a18ae5a449c7e96
Hena <meraey21@gmail.com>
2026-02-03 09:38:30 +0300

    feat: add reusable Card ui

---
+++ components/ui/Card.tsx
@@ -0,0 +1,15 @@
+
+type CardProps = {
+    children: React.ReactNode;
+    className?: string;
+}
+
+
+export default function Card({children,className = ""} : CardProps) {
+
+    return (
+      <div className={`bg-white rounded-2xl border border-slate-200 ${className}`}>
+        {children}
+      </div>
+    );
+}
