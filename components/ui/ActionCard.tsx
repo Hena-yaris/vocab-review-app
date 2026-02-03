de988e900eab3261c9f4b2e2bea64e82860a4778
Hena <meraey21@gmail.com>
2026-02-03 12:18:49 +0300

    feat: create ActionCard component

---
+++ components/ui/ActionCard.tsx
@@ -0,0 +1,26 @@
+import Card from "./Card";
+import { MainLink } from "@/data/mainLinks";
+import Link from "next/link";
+
+
+ const ActionCard = ({title,description,href,icon:Icon,iconColor,iconBg}: MainLink)=> {
+
+    return (
+      <Link href={href}>
+        <Card className="flex items-center p-6 gap-6 hover:shadow-xl transition-shadow duration-300">
+          <div
+            className={`flex items-center justify-center ${iconBg} p-4 rounded-full`}
+          >
+            <Icon className={`w-6 h-6 ${iconColor} `} />
+          </div>
+
+          <div>
+            <h3 className="font-semibold">{title}</h3>
+            <p className="text-slate-600 text-base">{description}</p>
+          </div>
+        </Card>
+      </Link>
+    );
+}
+
+export default ActionCard
